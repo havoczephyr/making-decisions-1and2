8f1cf1b07e4089845a8735594c35ae6bc7480197
Havoczephyr <havoczephyr@hotmail.com>
2021-10-13 14:50:22 -0400

    Initial Commit

---
+++ making-decisions.js
@@ -0,0 +1,12 @@
+let jonSnowAttack = 25;
+let jamieLannisterAttack = 35;
+
+//before the battle, Jon and Jamie compare attack levels to get insight to see who will win.
+
+if(jonSnowAttack > jamieLannisterAttack) {
+    console.log("Jon Snow has better attack than Jamie");
+} else if(jamieLannisterAttack > jonSnowAttack) {
+    console.log("Jamie has a better attack then Jon");
+} else {
+    console.log("Jon and Jamie have the same attack");
+};
